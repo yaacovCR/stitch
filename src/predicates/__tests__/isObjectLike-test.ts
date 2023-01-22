25ea116975e647d4e40fd849628dc1186be82854
Yaacov Rydzinski <yaacovCR@gmail.com>
2023-01-22 15:39:58 +0200

    add tests for isObjectLike (#10)

---
+++ src/predicates/__tests__/isObjectLike-test.ts
@@ -0,0 +1,25 @@
+import { expect } from 'chai';
+import { describe, it } from 'mocha';
+
+import { isObjectLike } from '../isObjectLike.js';
+
+describe('isObjectLike', () => {
+  it('should return `true` for objects', () => {
+    expect(isObjectLike({})).to.equal(true);
+    expect(isObjectLike(Object.create(null))).to.equal(true);
+    expect(isObjectLike(/a/)).to.equal(true);
+    expect(isObjectLike([])).to.equal(true);
+  });
+
+  it('should return `false` for non-objects', () => {
+    expect(isObjectLike(undefined)).to.equal(false);
+    expect(isObjectLike(null)).to.equal(false);
+    expect(isObjectLike(true)).to.equal(false);
+    expect(isObjectLike('')).to.equal(false);
+    expect(
+      isObjectLike(() => {
+        /* empty */
+      }),
+    ).to.equal(false);
+  });
+});
