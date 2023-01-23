d1fdb56c5612ccb10a1c96c02cdf1ef7ff75b418
GitHub Action Script <please@open.issue>
2023-01-23 13:31:06 +0000

    Deploy  to 'deno' branch

--- stitch/SuperSchema.ts
+++ stitch/SuperSchema.ts
@@ -34,9 +34,12 @@ import {
   GraphQLScalarType,
   GraphQLSchema,
   GraphQLUnionType,
+  isInputObjectType,
   isInputType,
+  isInterfaceType,
   isListType,
   isNonNullType,
+  isObjectType,
   isSpecifiedScalarType,
   Kind,
   OperationTypeNode,
@@ -68,12 +71,14 @@ const operations = [
  */
 export class SuperSchema {
   schemas: ReadonlyArray<GraphQLSchema>;
+  subschemaSetsByTypeAndField: ObjMap<ObjMap<Set<GraphQLSchema>>>;
   mergedRootTypes: ObjMap<GraphQLObjectType>;
   mergedTypes: ObjMap<GraphQLNamedType>;
   mergedDirectives: ObjMap<GraphQLDirective>;
   mergeSchema: GraphQLSchema;
   constructor(schemas: ReadonlyArray<GraphQLSchema>) {
     this.schemas = schemas;
+    this.subschemaSetsByTypeAndField = Object.create(null);
     this.mergedRootTypes = Object.create(null);
     this.mergedTypes = Object.create(null);
     this.mergedDirectives = Object.create(null);
@@ -114,6 +119,24 @@ export class SuperSchema {
           originalTypes[name] = types;
         }
         types.push(type);
+        if (
+          isObjectType(type) ||
+          isInterfaceType(type) ||
+          isInputObjectType(type)
+        ) {
+          let subschemaSetsByField = this.subschemaSetsByTypeAndField[name];
+          if (!subschemaSetsByField) {
+            subschemaSetsByField = Object.create(null);
+            this.subschemaSetsByTypeAndField[name] = subschemaSetsByField;
+          }
+          for (const fieldName of Object.keys(type.getFields())) {
+            let subschemaSet = subschemaSetsByField[fieldName];
+            if (!subschemaSet) {
+              subschemaSet = new Set();
+            }
+            subschemaSet.add(schema);
+          }
+        }
       }
       for (const directive of schema.getDirectives()) {
         const name = directive.name;
