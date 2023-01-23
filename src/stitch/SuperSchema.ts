4658d9bed4f82d66bc941f7d051ac0a133ebac5f
Yaacov Rydzinski <yaacovCR@gmail.com>
2023-01-23 15:10:03 +0200

    save which fields go to which subschema

--- src/stitch/SuperSchema.ts
+++ src/stitch/SuperSchema.ts
@@ -34,9 +34,12 @@ import {
   GraphQLScalarType,
   GraphQLSchema,
   GraphQLUnionType,
+  isInputObjectType,
   isInputType,
+  isInterfaceType,
   isListType,
   isNonNullType,
+  isObjectType,
   isSpecifiedScalarType,
   Kind,
   OperationTypeNode,
@@ -65,6 +68,7 @@ const operations = [
  */
 export class SuperSchema {
   schemas: ReadonlyArray<GraphQLSchema>;
+  subschemaSetsByTypeAndField: ObjMap<ObjMap<Set<GraphQLSchema>>>;
   mergedRootTypes: ObjMap<GraphQLObjectType>;
   mergedTypes: ObjMap<GraphQLNamedType>;
   mergedDirectives: ObjMap<GraphQLDirective>;
@@ -72,6 +76,7 @@ export class SuperSchema {
 
   constructor(schemas: ReadonlyArray<GraphQLSchema>) {
     this.schemas = schemas;
+    this.subschemaSetsByTypeAndField = Object.create(null);
     this.mergedRootTypes = Object.create(null);
     this.mergedTypes = Object.create(null);
     this.mergedDirectives = Object.create(null);
@@ -111,12 +116,33 @@ export class SuperSchema {
         if (name.startsWith('__')) {
           continue;
         }
+
         let types = originalTypes[name];
         if (!types) {
           types = [];
           originalTypes[name] = types;
         }
         types.push(type);
+
+        if (
+          isObjectType(type) ||
+          isInterfaceType(type) ||
+          isInputObjectType(type)
+        ) {
+          let subschemaSetsByField = this.subschemaSetsByTypeAndField[name];
+          if (!subschemaSetsByField) {
+            subschemaSetsByField = Object.create(null);
+            this.subschemaSetsByTypeAndField[name] = subschemaSetsByField;
+          }
+
+          for (const fieldName of Object.keys(type.getFields())) {
+            let subschemaSet = subschemaSetsByField[fieldName];
+            if (!subschemaSet) {
+              subschemaSet = new Set();
+            }
+            subschemaSet.add(schema);
+          }
+        }
       }
 
       for (const directive of schema.getDirectives()) {
