6d29be3fe9dd5df132a1520f0830ff0bc100ae71
GitHub Action Script <please@open.issue>
2023-01-24 09:21:41 +0000

    Deploy  to 'npm' branch

--- stitch/mapAsyncIterable.d.ts
+++ stitch/mapAsyncIterable.d.ts
@@ -1,9 +1,9 @@
 import type { PromiseOrValue } from '../types/PromiseOrValue.js';
 /**
- * Given an AsyncIterable and a callback function, return an AsyncIterator
+ * Given an AsyncIterable and a callback function, return an AsyncIterableIterator
  * which produces values mapped via calling the callback function.
  */
 export declare function mapAsyncIterable<T, U>(
-  iterable: AsyncIterable<T> | AsyncIterableIterator<T>,
-  callback: (value: T) => PromiseOrValue<U>,
+  iterable: AsyncIterable<T>,
+  fn: (value: T) => PromiseOrValue<U>,
 ): AsyncIterableIterator<U>;
