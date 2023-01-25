03678057219b1f8e656112409f80ab0a24b3d14b
Yaacov Rydzinski <yaacovCR@gmail.com>
2023-01-25 13:46:29 +0200

    fix null merging

--- src/stitch/__tests__/execute-test.ts
+++ src/stitch/__tests__/execute-test.ts
@@ -3,6 +3,8 @@ import type { GraphQLSchema } from 'graphql';
 import { buildSchema, execute as graphQLExecute, parse } from 'graphql';
 import { describe, it } from 'mocha';
 
+import { expectJSON } from '../../__testUtils__/expectJSON.js';
+
 import { execute } from '../execute.js';
 import type { Subschema } from '../SuperSchema.js';
 
@@ -59,6 +61,54 @@ describe('execute', () => {
         someObject: { someField: 'someField' },
         anotherObject: { someField: 'someField' },
       },
+    });
+  });
+
+  it('works when data will be null for one', () => {
+    const someSchema = buildSchema(`
+      type Query {
+        someObject: SomeObject!
+      }
+
+      type SomeObject {
+        someField: String!
+      }
+    `);
+
+    const anotherSchema = buildSchema(`
+      type Query {
+        anotherObject: AnotherObject
+      }
+
+      type AnotherObject {
+        someField: String
+      }
+    `);
+
+    const someSubschema = getSubschema(someSchema, {
+      someObject: { someField: null },
+    });
+    const anotherSubschema = getSubschema(anotherSchema, {
+      anotherObject: { someField: 'someField' },
+    });
+
+    const result = execute({
+      subschemas: [someSubschema, anotherSubschema],
+      document: parse(
+        ' { someObject { someField } anotherObject { someField } } ',
+      ),
+    });
+
+    expectJSON(result).toDeepEqual({
+      data: null,
+      errors: [
+        {
+          message:
+            'Cannot return null for non-nullable field SomeObject.someField.',
+          locations: [{ line: 1, column: 17 }],
+          path: ['someObject', 'someField'],
+        },
+      ],
     });
   });
 });
