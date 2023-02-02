82315a2db8bd5b179174523741dc2e443660bad2
Yaacov Rydzinski <yaacovCR@gmail.com>
2023-02-02 14:47:17 +0200

    add support for lists
    note that it does not require any changing of plans!

--- src/stitch/__tests__/PlannedOperation-test.ts
+++ src/stitch/__tests__/PlannedOperation-test.ts
@@ -209,4 +209,209 @@ describe('PlannedOperation', () => {
       },
     });
   });
+
+  it('works to stitch root fields with lists', () => {
+    const someSchema = buildSchema(`
+      type Query {
+        someObject: [SomeObject]
+      }
+
+      type SomeObject {
+        someField: [String]
+      }
+    `);
+
+    const anotherSchema = buildSchema(`
+      type Query {
+        anotherObject: [AnotherObject]
+      }
+
+      type AnotherObject {
+        someField: [String]
+      }
+    `);
+
+    const someSubschema = getSubschema(someSchema, {
+      someObject: [{ someField: ['someField'] }],
+    });
+    const anotherSubschema = getSubschema(anotherSchema, {
+      anotherObject: [{ someField: ['someField'] }],
+    });
+    const superSchema = new SuperSchema([someSubschema, anotherSubschema]);
+
+    const operation = parse(
+      `{
+        someObject { someField }
+        anotherObject { someField }
+      }`,
+      { noLocation: true },
+    ).definitions[0] as OperationDefinitionNode;
+
+    const plannedOperation = createPlannedOperation(superSchema, operation);
+
+    expect(plannedOperation.execute()).to.deep.equal({
+      data: {
+        someObject: [
+          {
+            someField: ['someField'],
+          },
+        ],
+        anotherObject: [
+          {
+            someField: ['someField'],
+          },
+        ],
+      },
+    });
+  });
+
+  it('works to stitch subfields with lists', () => {
+    const someSchema = buildSchema(`
+      type Query {
+        someObject: [SomeObject]
+      }
+
+      type SomeObject {
+        someField: [String]
+      }
+    `);
+
+    const anotherSchema = buildSchema(`
+      type Query {
+        someObject: [SomeObject]
+        anotherField: [String]
+      }
+
+      type SomeObject {
+        anotherField: [String]
+      }
+    `);
+
+    const someSubschema = getSubschema(someSchema, {
+      someObject: [
+        { someField: ['someFieldA'] },
+        { someField: ['someFieldB'] },
+      ],
+    });
+    const anotherSubschema = getSubschema(anotherSchema, {
+      anotherField: ['anotherField'],
+    });
+    const superSchema = new SuperSchema([someSubschema, anotherSubschema]);
+
+    const operation = parse(
+      `{
+        someObject { someField anotherField }
+      }`,
+      { noLocation: true },
+    ).definitions[0] as OperationDefinitionNode;
+
+    const plannedOperation = createPlannedOperation(superSchema, operation);
+
+    expect(plannedOperation.execute()).to.deep.equal({
+      data: {
+        someObject: [
+          {
+            someField: ['someFieldA'],
+            anotherField: ['anotherField'],
+          },
+          {
+            someField: ['someFieldB'],
+            anotherField: ['anotherField'],
+          },
+        ],
+      },
+    });
+  });
+
+  it('works to stitch sub-subfields with lists', () => {
+    const someSchema = buildSchema(`
+      type Query {
+        someObject: [SomeObject]
+      }
+
+      type SomeObject {
+        someField: [SomeNestedObject]
+      }
+
+      type SomeNestedObject {
+        someNestedField: [String]
+      }
+    `);
+
+    const anotherSchema = buildSchema(`
+      type Query {
+        someObject: [SomeObject]
+        anotherNestedField: [String]
+      }
+
+      type SomeObject {
+        someField: [SomeNestedObject]
+      }
+
+      type SomeNestedObject {
+        anotherNestedField: [String]
+      }
+    `);
+
+    const someSubschema = getSubschema(someSchema, {
+      someObject: [
+        {
+          someField: [
+            { someNestedField: ['someNestedFieldA'] },
+            { someNestedField: ['someNestedFieldB'] },
+          ],
+        },
+        {
+          someField: [
+            { someNestedField: ['someNestedField1'] },
+            { someNestedField: ['someNestedField2'] },
+          ],
+        },
+      ],
+    });
+    const anotherSubschema = getSubschema(anotherSchema, {
+      anotherNestedField: ['anotherNestedField'],
+    });
+    const superSchema = new SuperSchema([someSubschema, anotherSubschema]);
+
+    const operation = parse(
+      `{
+        someObject { someField { someNestedField anotherNestedField } }
+      }`,
+      { noLocation: true },
+    ).definitions[0] as OperationDefinitionNode;
+
+    const plannedOperation = createPlannedOperation(superSchema, operation);
+
+    expect(plannedOperation.execute()).to.deep.equal({
+      data: {
+        someObject: [
+          {
+            someField: [
+              {
+                someNestedField: ['someNestedFieldA'],
+                anotherNestedField: ['anotherNestedField'],
+              },
+              {
+                someNestedField: ['someNestedFieldB'],
+                anotherNestedField: ['anotherNestedField'],
+              },
+            ],
+          },
+          {
+            someField: [
+              {
+                someNestedField: ['someNestedField1'],
+                anotherNestedField: ['anotherNestedField'],
+              },
+              {
+                someNestedField: ['someNestedField2'],
+                anotherNestedField: ['anotherNestedField'],
+              },
+            ],
+          },
+        ],
+      },
+    });
+  });
 });
