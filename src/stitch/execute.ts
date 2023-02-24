b4fc8dd4e625651f47e33e429ceb09d34d24eb43
Yaacov Rydzinski <yaacovCR@gmail.com>
2023-02-24 10:41:47 +0200

    lint

--- src/stitch/execute.ts
+++ src/stitch/execute.ts
@@ -8,8 +8,8 @@ import type { PromiseOrValue } from '../types/PromiseOrValue.js';
 
 import type { ExecutionArgs } from './buildExecutionContext.js';
 import { buildExecutionContext } from './buildExecutionContext.js';
-import { Plan } from './Plan.js';
 import { Executor } from './Executor.js';
+import { Plan } from './Plan.js';
 
 export function execute(
   args: ExecutionArgs,
@@ -46,12 +46,7 @@ export function execute(
     fragmentMap,
   );
 
-  const executor = new Executor(
-    plan,
-    operation,
-    fragments,
-    rawVariableValues,
-  );
+  const executor = new Executor(plan, operation, fragments, rawVariableValues);
 
   return executor.execute();
 }
