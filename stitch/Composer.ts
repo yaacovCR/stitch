f639a65fd0501e4f15b2ca8dd0ceddaef15be726
GitHub Action Script <please@open.issue>
2023-06-15 13:14:41 +0000

    Deploy  to 'deno' branch

--- stitch/Composer.ts
+++ stitch/Composer.ts
@@ -9,11 +9,16 @@ import type { ObjMap } from '../types/ObjMap.ts';
 import type { PromiseOrValue } from '../types/PromiseOrValue.ts';
 import { isObjectLike } from '../predicates/isObjectLike.ts';
 import { isPromise } from '../predicates/isPromise.ts';
+import { AccumulatorMap } from '../utilities/AccumulatorMap.ts';
 import { PromiseAggregator } from '../utilities/PromiseAggregator.ts';
 import type { FieldPlan } from './FieldPlan.ts';
+import type { Subschema } from './SuperSchema.ts';
 type Path = ReadonlyArray<string | number>;
-interface Parent {
-  [key: string | number]: unknown;
+interface FetchPlan {
+  subschemaSelections: ReadonlyArray<SelectionNode>;
+  parent: ObjMap<unknown>;
+  target: ObjMap<unknown>;
+  path: Path;
 }
 /**
  * @internal
@@ -65,7 +70,7 @@ export class Composer {
     }
     return this.promiseAggregator.resolved().then(() => this._buildResponse());
   }
-  _createDocument(selections: Array<SelectionNode>): DocumentNode {
+  _createDocument(selections: ReadonlyArray<SelectionNode>): DocumentNode {
     return {
       kind: Kind.DOCUMENT,
       definitions: [
@@ -88,7 +93,7 @@ export class Composer {
       : { data: fieldsOrNull };
   }
   _handleMaybeAsyncResult(
-    parent: Parent | undefined,
+    parent: ObjMap<unknown> | undefined,
     fields: ObjMap<unknown>,
     fieldPlan: FieldPlan | undefined,
     result: PromiseOrValue<ExecutionResult>,
@@ -116,7 +121,7 @@ export class Composer {
     this.promiseAggregator.add(promise);
   }
   _handleResult(
-    parent: Parent | undefined,
+    parent: ObjMap<unknown> | undefined,
     fields: ObjMap<unknown>,
     fieldPlan: FieldPlan | undefined,
     result: ExecutionResult,
@@ -146,21 +151,42 @@ export class Composer {
       this._deepMerge(fields, key, value);
     }
     if (fieldPlan !== undefined) {
-      this._executeSubFieldPlans(
+      const subQueriesBySchema = new AccumulatorMap<Subschema, FetchPlan>();
+      this._collectSubQueries(
+        subQueriesBySchema,
         result.data,
         this.fieldPlan.subFieldPlans,
         path,
       );
+      for (const [subschema, subQueries] of subQueriesBySchema) {
+        for (const subQuery of subQueries) {
+          // TODO: send one document per subschema
+          const subResult = subschema.executor({
+            document: this._createDocument(subQuery.subschemaSelections),
+            variables: this.rawVariableValues,
+          });
+          this._handleMaybeAsyncResult(
+            subQuery.parent,
+            subQuery.target,
+            // TODO: add multilayer plan support
+            undefined,
+            subResult,
+            subQuery.path,
+          );
+        }
+      }
     }
   }
-  _executeSubFieldPlans(
+  _collectSubQueries(
+    subQueriesBySchema: AccumulatorMap<Subschema, FetchPlan>,
     fields: ObjMap<unknown>,
     subFieldPlans: ObjMap<FieldPlan>,
     path: Path,
   ): void {
     for (const [key, subFieldPlan] of Object.entries(subFieldPlans)) {
       if (fields[key] !== undefined) {
-        this._executePossibleListSubFieldPlan(
+        this._collectPossibleListSubQueries(
+          subQueriesBySchema,
           fields,
           fields[key] as ObjMap<unknown> | Array<unknown>,
           subFieldPlan,
@@ -169,16 +195,18 @@ export class Composer {
       }
     }
   }
-  _executePossibleListSubFieldPlan(
-    parent: Parent,
+  _collectPossibleListSubQueries(
+    subQueriesBySchema: AccumulatorMap<Subschema, FetchPlan>,
+    parent: ObjMap<unknown> | Array<unknown>,
     fieldsOrList: ObjMap<unknown> | Array<unknown>,
     fieldPlan: FieldPlan,
     path: Path,
   ): void {
     if (Array.isArray(fieldsOrList)) {
       for (let i = 0; i < fieldsOrList.length; i++) {
-        this._executePossibleListSubFieldPlan(
-          fieldsOrList as unknown as Parent,
+        this._collectPossibleListSubQueries(
+          subQueriesBySchema,
+          fieldsOrList,
           fieldsOrList[i] as ObjMap<unknown>,
           fieldPlan,
           [...path, i],
@@ -186,25 +214,23 @@ export class Composer {
       }
       return;
     }
-    this._executeSubFieldPlan(parent, fieldsOrList, fieldPlan, path);
-  }
-  _executeSubFieldPlan(
-    parent: Parent,
-    fields: ObjMap<unknown>,
-    fieldPlan: FieldPlan,
-    path: Path,
-  ): void {
     for (const [
       subschema,
       subschemaSelections,
     ] of fieldPlan.selectionMap.entries()) {
-      const result = subschema.executor({
-        document: this._createDocument(subschemaSelections),
-        variables: this.rawVariableValues,
+      subQueriesBySchema.add(subschema, {
+        subschemaSelections,
+        parent: parent as ObjMap<unknown>,
+        target: fieldsOrList,
+        path,
       });
-      this._handleMaybeAsyncResult(parent, fields, undefined, result, path);
     }
-    this._executeSubFieldPlans(fields, fieldPlan.subFieldPlans, path);
+    this._collectSubQueries(
+      subQueriesBySchema,
+      fieldsOrList,
+      fieldPlan.subFieldPlans,
+      path,
+    );
   }
   _deepMerge(fields: ObjMap<unknown>, key: string, value: unknown): void {
     if (
