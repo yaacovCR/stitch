da26a3465abc9fe1fe1c5dd5444dd6cff076dcc1
Yaacov Rydzinski <yaacovCR@gmail.com>
2023-06-29 13:46:16 +0300

    printPlan: small refactors (#120)

--- src/stitch/printPlan.ts
+++ src/stitch/printPlan.ts
@@ -4,6 +4,10 @@ import { Kind, print } from 'graphql';
 import type { FieldPlan, StitchTree, SubschemaPlan } from './Planner.js';
 import type { Subschema, SuperSchema } from './SuperSchema.js';
 
+function generateIndent(indent: number): string {
+  return ' '.repeat(indent);
+}
+
 export function printPlan(
   plan: FieldPlan,
   indent = 0,
@@ -11,9 +15,9 @@ export function printPlan(
 ): string {
   const superSchema = plan.superSchema;
   const entries = [];
-  const stitchTrees = Array.from(Object.entries(plan.stitchTrees));
+  const stitchTrees = Object.entries(plan.stitchTrees);
   if (plan.subschemaPlans.size > 0 || stitchTrees.length > 0) {
-    const spaces = new Array(indent).fill(' ', 0, indent).join('');
+    const spaces = generateIndent(indent);
 
     entries.push(
       `${spaces}${type === undefined ? 'Plan' : `For type '${type.name}'`}:`,
@@ -37,7 +41,7 @@ function printSubschemaPlans(
   subschemaPlans: Map<Subschema, SubschemaPlan>,
   indent: number,
 ): string {
-  return Array.from(subschemaPlans.entries())
+  return [...subschemaPlans.entries()]
     .map(([subschema, subschemaPlan]) =>
       printSubschemaPlan(superSchema, subschema, subschemaPlan, indent),
     )
@@ -50,7 +54,7 @@ function printSubschemaPlan(
   subschemaPlan: SubschemaPlan,
   indent: number,
 ): string {
-  const spaces = new Array(indent).fill(' ', 0, indent).join('');
+  const spaces = generateIndent(indent);
   const entries = [];
 
   const stitchTrees = Object.entries(subschemaPlan.stitchTrees);
@@ -83,18 +87,14 @@ function printSubschemaFieldNodes(
   fieldNodes: ReadonlyArray<FieldNode>,
   indent: number,
 ): string {
-  const spaces = new Array(indent).fill(' ', 0, indent).join('');
-  let result = '';
-  result += `${spaces}FieldNodes:\n`;
-  result += `${spaces}  `;
-  result += printSelectionSet(
+  const spaces = generateIndent(indent);
+  return `${spaces}FieldNodes:\n${spaces}  ${printSelectionSet(
     {
       kind: Kind.SELECTION_SET,
       selections: fieldNodes,
     },
     indent + 2,
-  );
-  return result;
+  )}`;
 }
 
 function printStitchTrees(
@@ -113,23 +113,21 @@ function printStitchTree(
   stitchTree: StitchTree,
   indent: number,
 ): string {
-  const spaces = new Array(indent).fill(' ', 0, indent).join('');
-  let stitchTreeEntry = '';
-  stitchTreeEntry += `${spaces}For key '${responseKey}':\n`;
-
+  const spaces = generateIndent(indent);
   const entries = [];
+  entries.push(`${spaces}For key '${responseKey}':`);
+
   for (const [type, fieldPlan] of stitchTree.fieldPlans) {
     entries.push(printPlan(fieldPlan, indent + 2, type));
   }
 
-  stitchTreeEntry += entries.join('\n');
-  return stitchTreeEntry;
+  return entries.join('\n');
 }
 
 function printSelectionSet(
   selectionSet: SelectionSetNode,
   indent: number,
 ): string {
-  const spaces = new Array(indent).fill(' ', 0, indent).join('');
+  const spaces = generateIndent(indent);
   return print(selectionSet).split('\n').join(`\n${spaces}`);
 }
